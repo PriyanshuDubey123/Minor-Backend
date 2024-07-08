963dd03f7cae067aed9e4854ba7c77858b0f72d0
PriyanshuDubey123 <priyanshudube397@gmail.com>
2024-07-08 22:38:49 +0530

    fixings

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ const {totalAmount} = req.body;
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/studymate");
+  await mongoose.connect(process.env.MONGODB_URL);
   console.log("database connected");
 }
 
